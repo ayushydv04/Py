a743863666d88ab3cc95f04e4c42913e1a83a5f8
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-15 10:00:00 +0530

    update git commit -m "update"

---
+++ 3datatypes.py
@@ -0,0 +1,21 @@
+# TYPES OF DATATYPES IN PYTHON
+# numbers,strings,booleans,lists,tuples,sets,dictonaries,int,float 
+a=56
+b="jimmy"
+c=23+45j
+print(type(a))
+print(type(b))
+print(type(c))
+
+# TYPE CONVERSION
+x=str(a)
+print(type(x)) 
+# changed variable ko print karwana hai
+
+# concatinate can be done only with same datatypes
+a="jimmy"
+b="is jolly"
+c=99
+print(a+b)
+print(a+str(c))   #but if we change the datatypes we can concatinate
+
