23967a9e641905bf04d48e4fb20a738c17fdff2f
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-03-10 10:51:00 +0530

    update

---
+++ 1comments.py
@@ -0,0 +1,10 @@
+print("jimmy") 
+print("this is new tutorial")
+# ab agar isse comment krna hai to ya to '#' lgaa do aage ya shortcut is ctrl+/
+# multi line comment ke liye triple single quotes use krte hai
+x="jimmy"
+print(x[::-1])
+
+
+# \n se jo bhi text hoga vo next lline m aa jaayega 
+# agar '  ' ke andar bhi dubara double quotes daalne h to \'  '\, backslash daalna padega 
