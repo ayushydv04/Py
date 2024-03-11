86f999b619b6d8ac6914e390629027c354318c3b
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-03-11 10:38:49 +0530

    update

---
+++ 5operators.py
@@ -0,0 +1,14 @@
+# TYPES
+# arithmetic    +,-,*,/
+# assignment    +=.-=,/=
+# comparison    true false
+# logical        and, or, not
+
+a=56
+b=7
+print("the value of a+b is",a+b)
+print("the value of a-b is",a-b)
+print("the value of a*b is",a*b)
+print("the value of a/b is",a/b)
+print("the value of a%b is",a%b)
+print("the value of a//b is",a//b)
