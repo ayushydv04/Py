1a9c7819fc7e350379f09e78aa97a4bb63cf7da5
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-03-15 06:28:27 +0530

    Initial commit

---
+++ 2variables.py
@@ -0,0 +1,8 @@
+a=3
+b=5
+print(a)
+print ("this os the value of a and b", a,b)
+
+# variable cannot start with number
+# variables are case sensitive
+# variables only start with letters and underscore
